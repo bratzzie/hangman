0d60425afc3486b9bf58426f38516f093775be9d
Bichurina Anastasiia <69593629+bratzzie@users.noreply.github.com>
2021-02-28 23:30:26 +0200

    configured main functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+import Figure from './components/Figure';
+import Header from './components/Header'
+import Word from './components/Word';
+import WrongLetters from './components/WrongLetters';
+
+const words = ['application', 'programming', 'interface', 'react']
+
+let selectedWord = words[Math.floor(Math.random() * words.length)]
+
 function App() {
+  const [playable, setPlayable] = useState(true)
+  const [correctLetters, setCorrectLetters] = useState([])
+  const [wrongLetters, setWrongLetters] = useState([]);
+
+  useEffect(() => {
+   const handleKeydown = event => {
+     const {key, keyCode} = event
+     if( playable && keyCode >= 65 && keyCode <= 90){
+       const letter = key.toLowerCase()
+
+       if(selectedWord.includes(letter)){
+         if(!correctLetters.includes(letter)){
+           setCorrectLetters(currentLetters => [...currentLetters, letter])
+         }
+         else{
+           // show notification
+         }
+        } else{
+           if( !wrongLetters.includes(letter)){
+             setWrongLetters(wrongLetters => [...wrongLetters, letter])
+           } else{
+             // show notification
+           }
+         }
+       }
+     }
+   window.addEventListener('keydown', handleKeydown)
+
+   return () => window.removeEventListener('keydown', handleKeydown)
+  }, [correctLetters, wrongLetters, playable])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header />
+        <div className="game-container">
+           <Figure wrongLetters={wrongLetters} />
+           <WrongLetters wrongLetters={wrongLetters}/>
+           <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+        </div>
+       
     </div>
   );
 }
